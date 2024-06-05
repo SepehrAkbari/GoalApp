3c1ecc4656f4b517fe933749db0035a93870291e
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-06-05 15:16:28 +0330

    Create goalData.js

---
+++ src/goalData.js
@@ -0,0 +1,16 @@
+const initialGoalData = {
+  goal_name: "Learn Python",
+  deadline: "2024-11-20",
+  tasks: [
+    {
+      task_name: "take a python course",
+      deadline: "2024-08-20",
+    },
+    {
+      task_name: "practice real-life python projects",
+      deadline: "2024-11-10",
+    },
+  ],
+};
+
+export default initialGoalData;
